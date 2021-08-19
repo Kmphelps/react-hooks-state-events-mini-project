08d8f3c8eb6361098177639e9281ab7475e3707d
Kayla Phelps <kmphelps822@gmail.com>
2021-08-18 19:05:26 -0700

    commit tree 9d46ad0660f60de89b52992da28c82311a57fc18

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,18 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+
+
+  const selectableCategories = categories.filter((category) => (  // excludes 'All' from the create new task dropdown
+    category !== "All"
+  ));
+
+  const optionElements = selectableCategories.map((category) => (  // adds a category dropdown option for each cateory
+    <option>{category}</option>
+  ));
+
+
+  function handleText(e) { // updates the text value to match the input value (see below)
+    setText(e.target.value)
+  }
+
+
+  function handleCategory(e) { // updates the category value to match the category selection (see below)
+    setCategory(e.target.value)
+  }
+
+
+  function handleSubmit(e) {  // creates a new object for the task
+    e.preventDefault();
+    onTaskFormSubmit({text, category});
+  }
+
+
+
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={handleText} />
       </label>
+
+
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={handleCategory}>
+          {optionElements}
         </select>
       </label>
+
+
       <input type="submit" value="Add task" />
     </form>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Task() {
+function Task({text, category, deleteHandler}) {
+  
+  function clickHandler() {
+      deleteHandler(text);
+  };
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button onClick={clickHandler} className="delete">X</button>
     </div>
   );
 }
